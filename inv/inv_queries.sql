fb03a87fadd5612bd3c0cd12d873b0ef62d5b9b4
lin <lin@abc.com>
2025-08-05 09:32:17 +0800

    add wms queries and modify inv query

--- inv/inv_queries.sql
+++ inv/inv_queries.sql
@@ -1,1 +1,3 @@
 select * from mtl_system_items_b;
+
+select * from mtl_transaction_types;
---
+++ wms/wms_dispatched_task.sql
@@ -0,0 +1,7 @@
+select * from wms_dispatched_tasks;
+
+select * from wms_dispatched_tasks_hist;
+
+select * from fnd_user;
+
+select * from wms_label_requests_hist;
